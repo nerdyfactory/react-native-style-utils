49939d88541dfc7fbc9f35cbe1d799e0188c44d2
JonghunYu <yuzong@naver.com>
2017-07-22 14:57:39 +0900

    WIP

--- example/src/AnotherComponent.js
+++ example/src/AnotherComponent.js
@@ -3,7 +3,7 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
+console.log("start anotherone")
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -18,9 +18,18 @@ var {height, width} = Dimensions.get('window');
 export default class AnotherComponent extends Component {
   render() {
     return (
-      <View style={[S.customStyle, S.box]}>
+      <View style={styles.container}>
         <Text> This is aligned with flex-end and center</Text>
       </View>
     );
   }
 }
+
+console.log("another");
+console.log(S.customStyle);
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.flatten([S.customStyle, S.box]),
+  },
+});
+console.log("end anotherone")
